897de8491124061ef2c2825b644b3f3e43b5ab9d
Noan <noamwt@gmail.com>
2021-09-12 04:08:43 -0300

    creation initial project spa frontend

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/login">Entrar</router-link>
   </div>
   <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  display: grid;
+  justify-items: center;
+  margin-top: 15px;
 }
 
 #nav a {
@@ -25,6 +21,5 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <div class="container-outher">
+      <h1 class="paragraph">Entrar</h1>
+      <span class="container-login">
+        <input class="form-control" type="email" placeholder="Login"
+      /></span>
+      <span class="container-password"
+        ><input class="form-control" type="password" placeholder="Senha"
+      /></span>
+
+      <br />
+      <div class="b-login">
+        <button type="button" class="btn btn-primary btn-lg">Login</button>
+      </div>
+      <p>Você não possui conta? <a href="#">Crie uma conta</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+@font-face {
+  font-family: Poppins-Regular;
+  src: url("../assets/Fonts/poppins/Poppins-Regular.ttf");
+}
+
+@font-face {
+  font-family: Poppins-Bold;
+  src: url("../assets/Fonts/poppins/Poppins-Bold.ttf");
+}
+
+@font-face {
+  font-family: Poppins-Medium;
+  src: url("../assets/Fonts/poppins/Poppins-Medium.ttf");
+}
+.paragraph {
+  font-family: Poppins-Medium;
+  font-size: 22px;
+  padding-bottom: 2rem;
+}
+
+.container-outher {
+  display: grid;
+  justify-items: center;
+  margin: 10% 30%;
+}
+.b-login {
+  padding: 1rem;
+}
+.btn-primary {
+  background-color: #3f3f3f;
+  padding: 10px 50px;
+}
+</style>
